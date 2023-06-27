f2024c134eea2dde466fb05b3d118eb6938ff719
Obayda <obayda.alesawi@gmail.com>
2023-06-27 15:24:51 +0300

    add ar translation

---
+++ lang/ar/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'disk' => [
+        'missing' => 'القرص المحدد (:disk) مفقود.',
+    ],
+    'path' => [
+        'exists' => 'المسار المحدد [:path] موجود بالفعل.',
+        'missing' => 'المسار المحدد [:path] مفقود.',
+    ],
+];
